8bd1a7454ad263a1115958574fe8062289b362c6
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-23 21:25:00 -0500

    this is both enlightening and confusing

--- src/server.clj
+++ src/server.clj
@@ -1,1 +1,26 @@
-(ns ex)
+(ns exopaste.server
+    (:require [bidi.ring :refer [make-handler]]
+              [com.stuartsierra.component :as component]
+              [aleph.http :as http]
+              [ring.util.request :as req]
+              [ring.util.response :as res]
+              [ring.middleware.params :refer [wrap-params]]
+              [exopaste.view :as view]
+              [exopaste.store :as store]))
+
+(defrecord HttpServer [server]
+; This component wraps the aleph webserver from http/start-server,
+; and we pass it the result of calling handler that builds the bidi route 
+; handler, which maps urls to functions that produce the associated content
+    component/Lifecycle
+    (start [this]
+        (assoc this :server (http/start-server (handler (:store this)) {:port 8080})))
+    (stop [this]
+        (assoc this :server nil)))
+
+(defn make-server []
+    (map->HttpServer {}))
+
+(defn paste-handler [store request]
+    (let [paste (store/get-paste-by-uuid store (:uuid (:route-params request)))]))
+        
